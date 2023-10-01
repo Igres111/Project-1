75aa0331c6a046a17d6d340111eebaf804d961f6
Igres111 <sergikaralashvili@gmail.com>
2023-10-01 23:52:10 +0400

    Finished Header

--- Basic-Project/src/components/Header.jsx
+++ Basic-Project/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import svgJar from "../assets/illustration-financial-freedom.svg";
-
+import svgHeroesLeft from "../assets/illustration-hero-left.svg";
+import svgHeroesRight from "../assets/illustration-hero-right.svg";
+import svgScroll from "../assets/icon-scroll.svg";
+import style from "./Header.module.css";
 function Header() {
   return (
-    <div>
-      <img src={svgJar} alt="" />
+    <div className={style.box}>
+      <img src={svgHeroesLeft} alt="Not found" />
+      <div className={style.innerBox}>
+        <h1 className={style.header}>
+          {" "}
+          Get paid for the work you <span className={style.span}>love</span> to
+          do.
+        </h1>
+        <p className={style.text}>
+          The 9-5 grind is so last century. We believe in living life on your
+          own terms. Whether you’re looking to escape the rat race or set up a
+          side hustle, we’ve got you covered.
+        </p>
+        <img className={style.scroll} src={svgScroll} alt="Not Found" />
+      </div>
+      <img src={svgHeroesRight} alt="Not Found" />
     </div>
   );
 }
